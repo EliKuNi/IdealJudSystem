aeffc5ff493e856d09c477320cd099cdd6a92fa4
Elizabeth <65304506+EliSauKu@users.noreply.github.com>
2021-10-07 16:25:14 +0200

    Initial commit

---
+++ functions.js
@@ -0,0 +1,28 @@
+
+
+window.onload=function(){
+Swal.fire({
+    icon: "info",
+    title: '✴️ Hello and welcome to my ideal Judicial system page! ✴️\n\nClick on the different colour buttons and I will present my proposals and ideas in detail.',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
+}
+// Redirects you to the ideal system, explained in detail :D
+
+$ = function(id) {
+  return document.getElementById(id);
+}
+
+var show = function(id) {
+	$(id).style.display ='block'; bvvvvvvvvvvvvvb
+}
+var hide = function(id) {
+	$(id).style.display ='none';
+}
+
+
